b699204fc244aca7b31f353f1d6161eef2a7795b
hcf1425 <hcf1425@163.com>
2018-09-22 19:55:01 +0800

    01_百度首页数据html文件

--- day01_requests基本使用/01_baidu_index.py
+++ day01_requests基本使用/01_baidu_index.py
@@ -10,9 +10,13 @@ url = 'http://www.baidu.com'
 # 发起请求获得响应结果
 response = requests.get(url)
 response.encoding = 'utf-8'
-# 打印响应内容
-print(response.text)
-print(response.encoding)
+# # 打印响应内容
+# print(response.text)
+# print(response.encoding)
+
+print(len(response.text))
+with open('baidu_index_with_no_requests_header.html','w') as f:
+    f.write(response.text)
 
 
 
